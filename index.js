abe9e5f25b7e10f78b40e3af105a4b153a3bd2c3
Ashutosh <ashutosh.immanent@gmail.com>
2022-03-11 16:05:13 +0530

    test

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 'use strict';
 
 var fs = require('fs');
-var path = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-
-  callback(null, result);
-};
+//App config
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
 
 
 
+//routes
+app.use("/",  (req, res) => {
+    res.json('hello')
+});
 
+
+mongoose.connect(
+	process.env.CONNECTION_URL,
+	{
+		useNewUrlParser: true,
+		// useCreateIndex: true,                                    
+		// useUnifiedTopology: true,
+		// useFindAndModify: false,
+	},
+	(err) => {
+		if (err) throw err;
+		console.log("connected to mongodb");
+	},
+);
+
+// app.get = function(event, context, callback) {
+//   var contents = fs.readFileSync(`public${path.sep}index.html`);
+//   var result = {
+//     statusCode: 200,
+//     body: contents.toString(),
+//     headers: {'content-type': 'text/html'}
+//   };
+
+//   callback(null, result);
+// };
+
+//listener
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static("frontend/build"));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+// 	});
+// }
+
+//listener
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log(`listening port localhost : ${port}`);
+});
