00ffe8cbc1c041453a76c7302aa90f603748bd7a
Ashutosh <ashutosh.immanent@gmail.com>
2022-03-11 15:12:48 +0530

    7878

--- index.js
+++ index.js
@@ -14,50 +14,6 @@ exports.get = function(event, context, callback) {
   callback(null, result);
 };
 
-// require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cookieParser = require("cookie-parser");
-// const path = require("path");
 
-// //App config
-// const app = express();
-// app.use(express.json({ limit: '50mb' }));
-// app.use(cookieParser());
-
-
-
-// //routes
-// app.use("/",  (req, res) => {
-//     res.json('hello')
-// });
-
-
-// mongoose.connect(
-// 	process.env.CONNECTION_URL,
-// 	{
-// 		useNewUrlParser: true,
-// 		// useCreateIndex: true,                                    
-// 		// useUnifiedTopology: true,
-// 		// useFindAndModify: false,
-// 	},
-// 	(err) => {
-// 		if (err) throw err;
-// 		console.log("connected to mongodb");
-// 	},
-// );
-// //listener
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static("frontend/build"));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-// 	});
-// }
-
-// //listener
-// const port = process.env.PORT;
-// app.listen(port, () => {
-// 	console.log(`listening port localhost : ${port}`);
-// });
 
 
